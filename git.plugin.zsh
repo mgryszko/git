83557d95a8ef0b2fdf2befd4b271f984272ce0c7
Marcin Gryszko <mgryszko@gmail.com>
2024-01-10 20:37:36 +0100

    Modernize git aliases

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 alias grhh1='git reset --hard HEAD~1'
-alias grhom='git reset --hard origin/master'
-alias gupp='git pull --rebase -p'
+alias grhom='git reset --hard origin/$(git_main_branch)'
+alias gprap='git pull --rebase --auto-stash -p'
 alias gcob='git checkout -b'
 alias gtl='git describe $(git rev-list --tags --max-count=1)'
 alias gfom='git fetch origin $(git_main_branch):$(git_main_branch)'
