420f4b8802cd689b4a553ad0d7ad7e0a8fb3bdc2
Marcin Gryszko <mgryszko@gmail.com>
2024-01-11 07:35:55 +0100

    Fix attribute name

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -2,7 +2,7 @@
 
 alias grhh1='git reset --hard HEAD~1'
 alias grhom='git reset --hard origin/$(git_main_branch)'
-alias gprap='git pull --rebase --auto-stash -p'
+alias gprap='git pull --rebase --autostash -p'
 alias gcob='git checkout -b'
 alias gtl='git describe $(git rev-list --tags --max-count=1)'
 alias gfom='git fetch origin $(git_main_branch):$(git_main_branch)'
